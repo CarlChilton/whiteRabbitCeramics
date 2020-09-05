4f0651ed0fc002983dd2404a3c6c0bc752c619fd
Carl Chilton <CarlChilton@yahoo.co.uk>
2020-09-05 19:53:59 +0100

    Removed build ignore

---
+++ feed-your-head/build/precache-manifest.dfc8d5e7495a533eaf3b7f8e3fe5c9b2.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10ef85575591b114f5b713595afccbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fa3e637c8a6bc42dd2c",
+    "url": "/static/css/main.be4dc2c7.chunk.css"
+  },
+  {
+    "revision": "18fe977fc12d08bb65e5",
+    "url": "/static/js/2.5db2f390.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/static/js/2.5db2f390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa3e637c8a6bc42dd2c",
+    "url": "/static/js/main.105ea25b.chunk.js"
+  },
+  {
+    "revision": "deaf17d855cbdaf7344d",
+    "url": "/static/js/runtime-main.47788c5d.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "f7cd1e7267abff735ed34913a9543751",
+    "url": "/static/media/banner.f7cd1e72.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "3b3f55bdceb172fc2884efd44a043fe0",
+    "url": "/static/media/grass1.3b3f55bd.jpg"
+  },
+  {
+    "revision": "48718866cd0e4886a57518592f0c513d",
+    "url": "/static/media/grass2.48718866.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "24fcaaecd49a1afadfb4ae08f9d7dd08",
+    "url": "/static/media/logo.24fcaaec.jpg"
+  },
+  {
+    "revision": "083d02d971c5978b3cf87e1bc6c864e0",
+    "url": "/static/media/lotsopots.083d02d9.jpg"
+  },
+  {
+    "revision": "0e7e5dfd01a12186e28592ba7773bb60",
+    "url": "/static/media/lotsopots2.0e7e5dfd.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  }
+]);
